ff81566dec9e6fae6934ac807083c4dd15c798fb
ForwarD_NerN <mlun42941@gmail.com>
2024-04-10 22:11:22 +0500

    Remove maxPlayerBotCap restriction

--- src/main/java/ru/nern/carpetlantern/CarpetLanternSettings.java
+++ src/main/java/ru/nern/carpetlantern/CarpetLanternSettings.java
@@ -1,16 +1,33 @@
 package ru.nern.carpetlantern;
 
 
+import carpet.api.settings.CarpetRule;
 import carpet.api.settings.Rule;
+import carpet.api.settings.Validator;
+import net.minecraft.server.command.ServerCommandSource;
 
 import static carpet.api.settings.RuleCategory.CREATIVE;
 import static carpet.api.settings.RuleCategory.FEATURE;
 import static carpettisaddition.CarpetTISAdditionSettings.TIS;
 
 public class CarpetLanternSettings {
+    private static class MaxBotCapValidator extends Validator<Integer>
+    {
+        @Override
+        public Integer validate(ServerCommandSource source, CarpetRule<Integer> currentRule, Integer newValue, String string)
+        {
+            return newValue >= 0 ? newValue : null;
+        }
+
+        @Override
+        public String description() { return "You must choose a value from 1 to 20M";}
+    }
+
     @Rule(
             options = {"2", "4"},
-            categories = CREATIVE
+            strict = false,
+            categories = CREATIVE,
+            validators = MaxBotCapValidator.class
     )
     public static int maxPlayerBotCap = 2;
 
